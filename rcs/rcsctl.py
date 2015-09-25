95949497291498ee2cbd37471fa16059cb05bde3
159645 <159645@gmail.com>
2015-09-25 15:33:35 +0200

    rcs

--- rcs/rcsctl.py
+++ rcs/rcsctl.py
@@ -12,35 +12,61 @@
 # de centraliser les fichier de configuration dans un endroit du système 
 # ou sur une zone reseau. Avec une tache cron on pourra regulièrement 
 # extraire la dernière révision du fichier vers le fichier d'origine.
-#
 # si on veut gerer par exemple /etc/hosts
 # creation de RCSTOP/etc 
 # creation d'un lien symbolique /etc/RCS --> RCSTOP/etc
 # le fichier de revision sera stocke dans RCSTOP/etc/hosts,v
 #
+# Utiliser rcs:
+# ajouter un fichier a controler:
+# $ ci -u /etc/hosts
+# la commande a pour effet de créer le fichier RCS/hosts.v
+# le fichier /etc/hosts a perdu son droit en ecriture.
+# modifier un fichier: 
+# $ co -l /etc/hosts
+# l'option -l permet d'eviter une autre extraction durant la modification
+# apporter les modifications au fichier
+# $ ci -u /etc/hosts
+# voir l'historique des modification:
+# $ rlog /etc/hosts
+#
+# Utiliser le script:
+# /etc/rcsctl
+# modifier la valeur de TOP pour indiquer ou stocker les fichier controlés 
+# modifier la valeur de RCSLST pour indiquer l'emplacement de la liste des
+# fichiers a controler.
+# modifier la valeur de TEXTEDITOR pour indiquer l'editeur de texte a util
+# iser pour la modification des fichiers controles.
+# 
+# <...
+# TOP=/root
+# RCSLST=/etc/sysconfig/rcsctl
+# TEXTEDITOR=vim
+# ...>
+#
+# /etc/sysconfig/rcsctl
+# pour chaque fichier a controler ajouter une ligne au fichier avec le nom
+# complet du fichier
+#
+# <...
+# # check itself
+# /etc/sysconfig/rcs
+# # ajouter les fichiers a controler ci-dessous
+#
+# ...> 
+#
 # @run:  Creation de RCSTOP si innexistant 
-#        pour chaque fichier de la liste creation de l'arborescence 
+#        pour chaque fichier de /etc/sysconfig/rcsctl creation de l'arborescence 
 #        des dossiers et sous dossiers dans RCSTOP.
-#        Creation des liens symboliques dans le système de fichier
-#        pointant sur le dossier de RCSTOP correspondant et checkin 
-#        des nouveaux fichiers.
+#        Creation des liens symboliques dans le système de fichier pointant sur le 
+#        dossier de RCSTOP correspondant. 
+#        Enregistrement des nouveaux fichiers dans rcs.
 #        Checkout des fichiers deja controles par RCS.
 #        dryrun affiche un resumé des action mais ne fait rien
 #
 # @change: Permet d'editer un fichier et d'enregistrer les modifica
 #          tions dans rcs.
 #
-# 
-# ajouter un fichier a controler:
-# $ ci -u /etc/hosts
-# la commande a pour effet de créer le fichier RCS/hosts.v
-# le fichier /etc/hosts a perdu son droit en ecriture.
-# 
-# modifier un fichier: 
-# $ co -l /etc/hosts
-# l'option -l permet d'eviter une autre extraction durant la modification
-# apporter les modifications au fichier
-# $ ci -u /etc/hosts
 import os
 import sys
 import string
@@ -51,15 +77,31 @@ except ImportError:
    print 'yum -y install pexpect.noarch'
    sys.exit(1)
 
-# racine de rcs
-TOP='/root'
-RCSTOP=os.path.join(TOP, 'RCS')
+usage = ('''
+{0} run <option>
 
-# liste des fichiers a controler par rcs
-RCSLST='/etc/sysconfig/rcs'
+option:
+  -d | --dryrun    : ne fait rien affiche juste les action 
+  -k | --dryrunko  : idem si tout va mal
+  -b | --back      : on annule
 
-# editeur de texte
-TEXTEDITOR='vim'
+
+{0} print <item>   : affiche les elements demande ou tous
+
+item:
+  dir : affiche les dossiers de RCSTOP
+  fic : affiche les fichiers controlés par rcs 
+  lnk : affiche les liens ajoutés au systeme
+  cin : affiche les fichiers qui seront ajoutes a rcs 
+  cout: affiche les fichiers a extraire de rcs
+
+{0} rlog <fichier> : affiche l'historique des modifications
+
+{0} change <fichier> <raison_du_changement>
+
+modifier un fichier et enregistre les modifications dans rcs
+
+'''.format(sys.argv[0]))
 
 class bcolors:
     OKBLUE = '\033[94m'
@@ -90,7 +132,7 @@ def printstat(lst=[], items=[]):
          else:
             for e in set(data[k]): info('[%s] %s' % (k,e))
 
-def run(top=RCSTOP, lst=[], dryrun=False, dryrunko=False, verbose=True):
+def run(top=None, lst=[], dryrun=False, dryrunko=False, verbose=True):
    ''' Creation de l'arborescence dans top, pour chaque fichier controlé.
        @systemfile: chemin complet du fichier a controler ex:"/etc/hosts"
        @systemfilename: nom du fichier a controler ex:"hosts"
@@ -101,7 +143,7 @@ def run(top=RCSTOP, lst=[], dryrun=False, dryrunko=False, verbose=True):
        @rcslink: chemin du lien vers top ex:"/etc/RCS" 
    '''
    #rcsbase = dict()
-   rcsbase = {'dir':[], 'fic':[], 'lnk':[], 'cin': [()], 'cout':[]} 
+   rcsbase = {'dir':[], 'fic':[], 'lnk':[], 'cin':[()], 'cout':[()]} 
 
    if not os.path.isdir(top):
       if not dryrun:
@@ -122,8 +164,11 @@ def run(top=RCSTOP, lst=[], dryrun=False, dryrunko=False, verbose=True):
       rcsfilename = os.path.basename(rcsfile)
       rcslink = os.path.join(systemdir, 'RCS')
 
+      if not os.path.isfile(systemfile):
+         info('[fic][??] innexistant %s' % systemfile, status='ko', verbose=verbose)
+         continue
+      info('[fic][..] existe %s' % systemfile, verbose=verbose)
       rcsbase['fic'].append(systemfile)
-
       # verification des dossiers dans TOPRCS
       if not os.path.isdir(rcsdir):
          if not dryrun:
@@ -132,7 +177,6 @@ def run(top=RCSTOP, lst=[], dryrun=False, dryrunko=False, verbose=True):
       else:
          info('[dir][..] existe %s' % rcsdir, verbose=verbose)
       rcsbase['dir'].append(rcsdir)
-
       # verification des liens dans le systeme
       if not os.path.islink(rcslink):
          if not dryrun:
@@ -141,7 +185,6 @@ def run(top=RCSTOP, lst=[], dryrun=False, dryrunko=False, verbose=True):
       else:
          info('[lnk][..] existe %s --> %s' % (rcsdir, rcslink), verbose=verbose)
       rcsbase['lnk'].append(rcslink) 
-
       # check in des nouveaux fichiers
       if not os.path.isfile(rcsfile):
          if dryrun: exitstatus = 0
@@ -269,32 +312,16 @@ def rlog(lst=[], fic=None):
       printstat(lst=lst, items=['fic'])
 
 if __name__ == '__main__':
-   usage = ('''
-{0} run <option>
-
-option:
-  -d | --dryrun    : ne fait rien affiche juste les action 
-  -k | --dryrunko  : idem si tout va mal
-  -b | --back      : on annule
-
-
-{0} print <item>   : affiche les elements demande ou tous
-
-item:
-  dir : affiche les dossiers de RCSTOP
-  fic : affiche les fichiers controlés par rcs 
-  lnk : affiche les liens ajoutés au systeme
-  cin : affiche les fichiers qui seront ajoutes a rcs 
-  cout: affiche les fichiers a extraire de rcs
-
-{0} rlog <fichier> : affiche l'historique des modifications
-
-{0} change <fichier> <raison_du_changement>
-
-modifier un fichier et enregistre les modifications dans rcs
-
-'''.format(sys.argv[0]))
-
+   try:
+      with open('/etc/rcsctl', 'r') as conf: conf = conf.readlines()
+      for line in conf:
+         if line.startswith('TOP'): TOP=line.split('=')[1].strip()
+         if line.startswith('RCSLST'): RCSLST=line.split('=')[1].strip()
+         if line.startswith('TEXTEDITOR'): TEXTEDITOR=line.split('=')[1].strip()
+      RCSTOP=os.path.join(TOP, 'RCS')
+   except IOError, e:
+      print e
+      sys.exit(e.errno)
    try:
       os.chdir(TOP)
    except OSError, e:
@@ -302,7 +329,7 @@ modifier un fichier et enregistre les modifications dans rcs
       sys.exit(e.errno)
    try:
       with open(RCSLST, 'r') as liste:
-        files = liste.readlines()
+        rcsfiles = liste.readlines()
    except IOError, e:
       print e
       sys.exit(e.errno)
@@ -310,29 +337,29 @@ modifier un fichier et enregistre les modifications dans rcs
    if len(sys.argv) > 1:
       if sys.argv[1] == 'run':
          if '-d' in sys.argv or '--dryrun' in sys.argv:
-            run(lst=files, dryrun=True, dryrunko=False)
+            run(top=RCSTOP, lst=rcsfiles, dryrun=True, dryrunko=False)
          elif '-k' in sys.argv or '--dryrunko' in sys.argv:
-            run(lst=files, dryrun=False, dryrunko=True)
+            run(top=RCSTOP, lst=rcsfiles, dryrun=False, dryrunko=True)
          elif '-b' in sys.argv or '--back' in sys.argv:
-            runback(lst=files)
+            runback(top=RCSTOP, lst=rcsfiles)
          else:
-            run(lst=files)       
+            run(top=RCSTOP, lst=rcsfiles)       
       elif sys.argv[1] == 'change':
          if len(sys.argv) < 3:
             print usage
             sys.exit(1)
          fichier = sys.argv[2]
          notification= ' '.join(sys.argv[3:])
-         change(lst=files, fic=fichier, notification=notification)
+         change(lst=rcsfiles, fic=fichier, notification=notification)
       elif sys.argv[1] == 'print':
          arg = sys.argv[2:]
-         printstat(lst=files, items=arg)
+         printstat(lst=rcsfiles, items=arg)
       elif sys.argv[1] == 'rlog':
          try:
             fichier = sys.argv[2]
          except IndexError:
             sys.exit(1)
-         rlog(lst=files, fic=fichier)
+         rlog(lst=rcsfiles, fic=fichier)
       else:
          print usage
          sys.exit(1)
